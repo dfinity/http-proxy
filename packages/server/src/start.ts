e74ae4e98fd770aee97dca9ccc8b9137c17427ec
Kepler Vital <119848388+keplervital@users.noreply.github.com>
2023-04-05 16:16:17 +0200

    improved error handling

--- packages/server/src/start.ts
+++ packages/server/src/start.ts
@@ -10,9 +10,18 @@ import {
 import { environment } from '~src/commons';
 import { ProxyServers } from './servers';
 
+process.on('uncaughtException', (err) => {
+  logger.error(`Uncaught Exception: ${String(err)}`);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  logger.error(`Unhandled rejection at reason: ${reason}`);
+  process.exit(1);
+});
+
 (async (): Promise<void> => {
   let servers: ProxyServers | null = null;
-
   try {
     if (!isSupportedPlatform(environment.platform)) {
       throw new UnsupportedPlatformError(environment.platform);
